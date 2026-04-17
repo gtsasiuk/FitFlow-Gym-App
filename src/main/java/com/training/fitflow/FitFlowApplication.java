6ddf621fb5cee1f8ef87c732067610b8192fbc91
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-04-18 01:33:37 +0300

    Initialize application context and retrieve GymFacade bean

--- src/main/java/com/training/fitflow/FitFlowApplication.java
+++ src/main/java/com/training/fitflow/FitFlowApplication.java
@@ -1,7 +1,17 @@
 package com.training.fitflow;
 
+import com.training.fitflow.config.AppConfig;
+import com.training.fitflow.facade.GymFacade;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class FitFlowApplication {
     static void main() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        GymFacade facade = context.getBean(GymFacade.class);
+
+        System.out.println(facade.getTrainee(1L));
 
     }
 }
