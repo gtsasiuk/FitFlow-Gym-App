f823c67637a2adcca99cef3d178a5d8d4093d827
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-04-20 20:43:07 +0300

    Add logging in GymFacade

--- src/main/java/com/training/fitflow/facade/GymFacade.java
+++ src/main/java/com/training/fitflow/facade/GymFacade.java
@@ -6,11 +6,13 @@ import com.training.fitflow.model.Training;
 import com.training.fitflow.service.TraineeService;
 import com.training.fitflow.service.TrainerService;
 import com.training.fitflow.service.TrainingService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Slf4j
 public class GymFacade {
     private final TraineeService traineeService;
     private final TrainerService trainerService;
@@ -23,43 +25,71 @@ public class GymFacade {
     }
 
     public Trainee createTrainee(Trainee trainee) {
-        return traineeService.create(trainee);
+        log.info("Facade: createTrainee request for {} {}", trainee.getFirstName(), trainee.getLastName());
+
+        Trainee result = traineeService.create(trainee);
+
+        log.info("Facade: trainee created id={}", result.getId());
+
+        return result;
     }
 
     public Trainee getTrainee(Long id) {
+        log.debug("Facade: getTrainee id={}", id);
         return traineeService.getById(id);
     }
 
     public List<Trainee> getAllTrainees() {
+        log.debug("Facade: getAllTrainees request");
         return traineeService.getAll();
     }
 
     public void deleteTrainee(Long id) {
+        log.info("Facade: deleteTrainee id={}", id);
         traineeService.deleteById(id);
     }
 
     public Trainer createTrainer(Trainer trainer) {
-        return trainerService.create(trainer);
+        log.info("Facade: createTrainer request for {} {}", trainer.getFirstName(), trainer.getLastName());
+
+        Trainer result = trainerService.create(trainer);
+
+        log.info("Facade: trainer created id={}", result.getId());
+
+        return result;
     }
 
     public Trainer getTrainer(Long id) {
+        log.debug("Facade: getTrainer id={}", id);
         return trainerService.getById(id);
     }
 
     public List<Trainer> getAllTrainers() {
+        log.debug("Facade: getAllTrainers request");
         return trainerService.getAll();
     }
 
-
     public Training createTraining(Training training) {
-        return trainingService.create(training);
+        log.info("Facade: createTraining request name={}, trainerId={}, traineeId={}",
+                training.getName(),
+                training.getTrainerId(),
+                training.getTraineeId()
+        );
+
+        Training result = trainingService.create(training);
+
+        log.info("Facade: training created id={}", result.getId());
+
+        return result;
     }
 
     public Training getTraining(Long id) {
+        log.debug("Facade: getTraining id={}", id);
         return trainingService.getById(id);
     }
 
     public List<Training> getAllTrainings() {
+        log.debug("Facade: getAllTrainings request");
         return trainingService.getAll();
     }
 }
