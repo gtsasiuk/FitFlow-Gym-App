f3f67ab1606fd701b6642bce296771abe8a99f9e
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-04-20 20:21:08 +0300

    Add logging in TrainerService

--- src/main/java/com/training/fitflow/service/TrainerService.java
+++ src/main/java/com/training/fitflow/service/TrainerService.java
@@ -6,40 +6,62 @@ import com.training.fitflow.util.PasswordGenerator;
 import com.training.fitflow.util.UsernameGenerator;
 import com.training.fitflow.util.UserUpdateUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TrainerService {
     private final TrainerDao dao;
     private final UsernameGenerator usernameGenerator;
     private final PasswordGenerator passwordGenerator;
 
     public Trainer create(Trainer trainer) {
+        log.info("Creating trainer: {} {}", trainer.getFirstName(), trainer.getLastName());
+
         String username = usernameGenerator.generate(trainer.getFirstName(), trainer.getLastName());
         String password = passwordGenerator.generate();
+
+        log.debug("Generated username={}", username);
 
         trainer.setUsername(username);
         trainer.setPassword(password);
         trainer.setActive(true);
 
-        return dao.save(trainer);
+        Trainer saved = dao.save(trainer);
+
+        log.info("Trainer created successfully with id={}", saved.getId());
+
+        return saved;
     }
 
     public Trainer update(Trainer trainer) {
+        log.info("Updating trainer id={}", trainer.getId());
+
         Trainer existingTrainer = getById(trainer.getId());
         UserUpdateUtil.updateNameFields(existingTrainer, trainer.getFirstName(), trainer.getLastName(), usernameGenerator);
         existingTrainer.setSpecialization(trainer.getSpecialization());
-        return dao.save(existingTrainer);
+
+        Trainer updated = dao.save(existingTrainer);
+
+        log.info("Trainer updated successfully id={}", updated.getId());
+
+        return updated;
     }
 
     public Trainer getById(Long id) {
-        return dao.findTrainerById(id).orElseThrow(() -> new RuntimeException("Trainer not found"));
+        log.debug("Fetching trainer by id={}", id);
+        return dao.findTrainerById(id).orElseThrow(() -> {
+            log.warn("Trainer not found id={}", id);
+            return new RuntimeException("Trainer not found");
+        });
     }
 
     public List<Trainer> getAll() {
+        log.debug("Fetching all trainers");
         return dao.findAllTrainers();
     }
 }
