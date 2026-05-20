8f18c7efd864015182ea3723f53a2ae264b88d0a
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-05-20 21:38:01 +0300

    Add swagger annotation for TrainingController

--- src/main/java/com/training/fitflow/dto/training/response/TraineeTrainingResponse.java
+++ src/main/java/com/training/fitflow/dto/training/response/TraineeTrainingResponse.java
@@ -1,7 +1,10 @@
 package com.training.fitflow.dto.training.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 
+@Schema(description = "Training information from trainee perspective")
 public record TraineeTrainingResponse(
         String trainingName,
         LocalDate trainingDate,
