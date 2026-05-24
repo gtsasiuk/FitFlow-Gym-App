70c308ddad37a9e1fd914c2d0f10352734ccea62
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-05-24 17:39:05 +0300

    Change FitFlowApplication

--- src/main/java/com/training/fitflow/FitFlowApplication.java
+++ src/main/java/com/training/fitflow/FitFlowApplication.java
@@ -1,16 +1,11 @@
 package com.training.fitflow;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class FitFlowApplication {
     public static void main(String[] args) {
-        log.info("Starting FitFlow application...");
         SpringApplication.run(FitFlowApplication.class, args);
-
-        log.info("Application finished execution");
     }
 }
