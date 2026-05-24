a87c18d47711380e9670343ece0fde9e3e3b0b89
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-05-24 20:37:19 +0300

    Little refactor in WebConfig

--- src/main/java/com/training/fitflow/config/WebConfig.java
+++ src/main/java/com/training/fitflow/config/WebConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.training.fitflow.security.BasicAuthInterceptor;
 import com.training.fitflow.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.Validator;
