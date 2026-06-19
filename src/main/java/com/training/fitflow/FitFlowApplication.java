48c65c0bd3637a86ca6c60b8fae330af246ca36e
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-06-20 01:16:51 +0300

    Configure eureka

--- src/main/java/com/training/fitflow/FitFlowApplication.java
+++ src/main/java/com/training/fitflow/FitFlowApplication.java
@@ -2,9 +2,13 @@ package com.training.fitflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 @EnableScheduling
 public class FitFlowApplication {
     public static void main(String[] args) {
